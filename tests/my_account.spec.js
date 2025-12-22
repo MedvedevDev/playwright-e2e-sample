ee30c6b17e60eaad805085b01d65f961950c72d5
MedvedevDev <a95.medvedev@gmail.com>
2025-12-22 20:39:58 +0100

    Mocking adress data

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -4,9 +4,33 @@ import { getLoginToken } from "../api-calls/getLoginToken";
 import { addCookieContext } from "../utils/addCookieContext";
 import { admin } from "../data/loginDetails";
 
-test.only("My account using cookie injection", async ({ page }) => {
+test.only("My account using cookie injection and mocking network request", async ({
+  page,
+}) => {
   // Make a request to get login token
   const token = await getLoginToken(admin.username, admin.password);
+
+  // MOCKING ERROR
+  // ** any url what match /api/user . doesnt matter what comes infront and after
+  // await page.route("**/api/user**", async (route, request) => {
+  //   route.fulfill({
+  //     status: 500,
+  //     contentType: "application/json",
+  //     body: JSON.stringify({ message: "ERROR FROM MOCKING" }),
+  //   });
+  // });
+
+  // MOCKING SOME ADRESS DATA
+  await page.route("**/api/address**", async (route, request) => {
+    const data = await route.fetch();
+    const json = await data.json();
+
+    json[0].firstName = "MOCKED NAME";
+    json[0].street = "MOCKED STREET";
+    await route.fulfill({
+      body: JSON.stringify(json),
+    });
+  });
 
   // Inject the login token into the browser
   const myAccount = new MyAccountPage(page);
@@ -22,4 +46,5 @@ test.only("My account using cookie injection", async ({ page }) => {
 
   await myAccount.visit();
   await myAccount.waitForPageHeading();
+  await page.pause();
 });
