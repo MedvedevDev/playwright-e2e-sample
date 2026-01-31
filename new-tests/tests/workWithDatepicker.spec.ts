ce472abd9aec4980cf4c06e4d406c9c4674d26c9
MedvedevDev <a95.medvedev@gmail.com>
2026-01-31 17:28:13 +0100

    Datepicker: select any date (dynamically)

--- new-tests/tests/workWithDatepicker.spec.ts
+++ new-tests/tests/workWithDatepicker.spec.ts
@@ -21,7 +21,9 @@ test("datepicker", async ({ page }) => {
   await expect(calendarInputField).toHaveValue("Jun 1, 2026");
 });
 
-test("dynamically select a date in the datepicker", async ({ page }) => {
+test("[ONLY within the current month] dynamically select a date in the datepicker", async ({
+  page,
+}) => {
   await page.getByText("Forms").click();
   await page.getByText("Datepicker ").click();
 
@@ -42,5 +44,43 @@ test("dynamically select a date in the datepicker", async ({ page }) => {
 
   // set the date
   await currentMonth.getByText(expectedDate, { exact: true }).click();
+  await expect(calendarInputField).toHaveValue(dateToAssert);
+});
+
+test("dynamically select a date in the datepicker", async ({ page }) => {
+  await page.getByText("Forms").click();
+  await page.getByText("Datepicker ").click();
+
+  // open the calendar
+  const calendarInputField = page.getByPlaceholder("Form Picker");
+  await calendarInputField.click();
+
+  let date = new Date();
+  date.setDate(date.getDate() + 14); // previous date
+  const expectedDate = date.getDate().toString();
+  const expectedMonthShort = date.toLocaleString("En-US", { month: "short" });
+  const expectedYear = date.getFullYear();
+
+  const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`;
+
+  let calendarMonthAndYear = await page
+    .locator("nb-calendar-view-mode")
+    .textContent();
+  const expectedMonthLong = date.toLocaleString("En-US", { month: "long" });
+  const expetedMonthAndYear = `${expectedMonthLong} ${expectedYear}`;
+  while (!calendarMonthAndYear.includes(expetedMonthAndYear)) {
+    await page
+      .locator('nb-calendar-pageable-navigation [data-name="chevron-right"]')
+      .click();
+    calendarMonthAndYear = await page
+      .locator("nb-calendar-view-mode")
+      .textContent();
+  }
+
+  // set the date
+  await page
+    .locator('[class="day-cell ng-star-inserted"]')
+    .getByText(expectedDate, { exact: true })
+    .click();
   await expect(calendarInputField).toHaveValue(dateToAssert);
 });
