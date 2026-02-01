d1cc19a2ace6e811b211991861737c7224f2cb98
MedvedevDev <a95.medvedev@gmail.com>
2026-02-01 17:30:52 +0100

    Wirking with iFrame and dragNdrop

---
+++ new-tests/tests/workWithDragAndDrop.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "@playwright/test";
+
+test("drag and drop within iFrame", async ({ page }) => {
+  await page.goto("https://www.globalsqa.com/demo-site/draganddrop/");
+  // locata iFrame
+  const iframeContainer = page.frameLocator(
+    '[rel-title="Photo Manager"] iframe',
+  );
+  await iframeContainer
+    .locator("li", { hasText: "High Tatras 2" })
+    .dragTo(iframeContainer.locator("#trash"));
+
+  //more procise control by mouse
+  await iframeContainer.locator("li", { hasText: "High Tatras 4" }).hover();
+  await page.mouse.down();
+  await iframeContainer.locator("#trash").hover();
+  await page.mouse.up();
+});
